ad1bdf81ba27a8d4abb96953e3c03d56f81614de
unknown <neerajarao2898@gmail.com>
2022-06-30 16:08:49 +0530

    Pre2

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # project-2-open-to-intern
 Project 2
+       
--- src/controllers/collegeController.js
+++ src/controllers/collegeController.js
@@ -13,25 +13,6 @@ const createCollege = async function(req,res){
             res.status(400).send({status: false, message: "you forget to fill all of these fields"})
             return
         }
-        // **************************
-        // let x = function(data){
-        //     let collArr =["name", "fullName", "logoLink"]
-        //     for (let i = 0; i < collArr.length;i++){
-        //         let cData = "";
-        //         if(!Object.keys(data).includes(collArr[i])){
-        //                 cData = cData+ ' '+(collArr[i])
-        //         }
-                
-        //         if(cData){
-        //             return  `Provide ${cData} Fields`
-        //         }
-        //         if(collArr[i] = ""){
-        //             cData= collArr[i] + "is required"
-        //         }
-        //      }
-        //     }
-        //     console.log(x(name))
-        // **************************
 
 
         if(!name){
@@ -82,56 +63,6 @@ const createCollege = async function(req,res){
 
 // ***********************************************************************************************
 
-// const getCollegeDetails = async function(req, res){
-
-//     try{
-//         const name = req.query.collegeName;
-
-//         if(!name){
-//             return res.status(400).send({stats:false, message:"Please Provide College Name"});
-//         }
-//         const find = await collegeModel.findOne({name:name, isDeleted:false}).select({_id:1}); // id
-
-//         const data = await collegeModel.findById(find).select({_id:0, name:1, fullName:1, logoLink:1});
-
-//         if(!data){
-//             return res.status(404).send({sataus:false, message: "No college Exits with the Name" })
-//         }
-        
-// //          const match=await internModel.find({collegeId:find._id, isDeleted:false}).select({_id:1,name:1,email:1,mobile:1});
-//         const internInfo = await internModel.find().populate('collegeId')
-//         if(internInfo.length == 0){
-//              return res.status(404).send({status:false, message:"No Intern Found For This College" })
-//          }
-//        console.log(internInfo)
-//         if(match.length == 0){
-//             return res.status(404).send({status:false, message:"No Intern Found For This College" })
-//         }
-//         // console.log(match)
-//         // const collegeDetails = JSON.parse(JSON.stringify(data))
-//         // collegeDetails.interns=match
-
-//         res.status(200).send({status:true, data:collegeDetails})
-//      }
-//     catch (err){
-//         res.status(500).send({status:false, message:err.message});
-
-//     }
-// };
-
-
-// const getCollegeDetails  = async function (req , res){
-//     try{
-//         let collegeName = req.query.collegeName ;
-//         let x = await collegeModel.find({name :collegeName})
-//         if(!x[0]){
-//             res.status(400).send({status : false ,  message: "Collehge not found"})
-//             return
-//         }
-//         let y = await internModel.find().populate('')
-//     }
-// }
-        // const data = await collegeModel.find({name : name , isDeleted : false})
 
         const getCollegeDetails = async function(req, res){
 
