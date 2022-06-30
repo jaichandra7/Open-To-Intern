7b2c3aa7af56f4089f21e783f7938679bf32a177
Jai chandra <jaichandra7@github.com>
2022-07-01 00:40:57 +0530

    Modified Messages

--- src/controllers/collegeController.js
+++ src/controllers/collegeController.js
@@ -14,41 +14,48 @@ const createCollege = async function(req,res){
             res.status(400).send({status: false, message: "you forget to fill all of these fields"})
             return
         }
-
+        //Values Cannot Be Empty
 
         if(!name){
             res.status(400).send({status: false, message: "Oops! you forget to enter name of your College"})
             return
         }
+        //Please Enter The College Name
 
         if(typeof name !=="string" || name.trim().length == 0){
             res.status(400).send({status: false, message: "Enter the college name properly "})
             return
         }
+        //Enter the College Name In String
         if(!validName.test(name)){
             res.status(400).send({status: false, message: " Use Characters Only !!"})
             return
         }
+        //Only characters are allowed
         if(!fullName){
             res.status(400).send({status: false, message: "Oops! you forget to enter Full Name of your College"})
             return
         }
+        //Please Provide Full Name Of The College
 
         if(typeof fullName !=="string" || fullName.trim().length == 0){
             res.status(400).send({status: false, message: "Enter the college fullName properly "})
             return
         }
+        //Please Enter The Full Name In String
 
         if(!logoLink){
             res.status(400).send({status: false, message: "Oops! you forget to enter LOGO URL of your College"})
             return
         }
+        //Please Provide The Logo Url
 
 
          if(typeof logoLink !=="string" || logoLink.trim().length == 0){
             res.status(400).send({status: false, message: "Make sure the LOGO URL is correct or not??"})
             return
         }
+        //The Logo Must Be In String and Not Blank
 
         const collegeData = await collegeModel.create(data)
         return res.status(201).send({status: true, data: collegeData})
@@ -73,12 +80,14 @@ const createCollege = async function(req,res){
                     res.status(400).send({status: false, message: "Query cannot be Empty"})
                     return
                 }
+                //Blank Query Is Not Accepted!
 
                 let collegeName = req.query.collegeName;
                 const collegeData = await collegeModel.findOne({name: collegeName , isDeleted: false})
                 // console.log(collegeData)
 
                 if (!collegeData)return res.status(400).send({ status: false, message: "enter a valid college name" })
+                //Entered College NAme Is Not Valid
                 
                 const collegeDetails = {
                     name: collegeData.name,
@@ -96,6 +105,7 @@ const createCollege = async function(req,res){
                 }
 
                 if(getInterns.length == 0)return res.status(400).send({ status: false, message: "No interns found" })
+                //Interns Value Is Empty //No Interns Found
             }
             catch (err) {
                 res.status(500).send({ status:false , error: err.message})
--- src/controllers/internController.js
+++ src/controllers/internController.js
@@ -21,56 +21,67 @@ const addIntern = async function(req,res){
             res.status(400).send({status: false, message: "Please provide your name !!"})
             return
         }
+        //Please Provide Intern Name 
 
         if(typeof name !== "string" || name.trim().length == 0){
             res.status(400).send({status: false, message: "Enter the Intern name properly "})
             return
         }
+        //Intern Name Must Not Be Empty , Should be In String 
 
         if(!email){
             res.status(400).send({status: false, message: "Oops! you forget enter email ID"})
             return
         }
+        //Please Provide The Email Id
         
         let checkEmail = Validator.validate(email)
         if (!checkEmail){
             res.status(400).send({status: false, message: "Make sure the email is in right format "})
             return
         }
+        //Provided Email Is Not In A Format
 
         let uniqueEmail = await internModel.findOne({email})
         if (uniqueEmail){
             res.status(400).send({status: false, message: "Sorry! this email is already exist"})
             return
         }
+        //This Email Is Already Registered
 
         if(!mobile){
             res.status(400).send({status: false, message: "Mobile Number can't be Empty"})
             return
         }
+        //Please Provide Mobile Number
 
         let uniqueMob = await internModel.find({mobile})
         if (uniqueMob[0]){
             res.status(400).send({status: false, message: "Sorry! this Mobile Number  is already exist"})
             return
         }
+        //Entered Mobile Number Is Already Registered
 
         if(!phoneNo.test(mobile)){
             res.status(400).send({status: false, message: "Use a valid Mobile Number"})
             return
         }
+        //Not A Valid Mobile Number 
 
 
         if(!collegeId){
             res.status(400).send({status: false, message: "CollegeId can't be Empty"})
             return
         }
+        //Please Provide Intern's College Id // Blank College ID Is Not Accepted
 
         let uniqueCollegeId = await internModel.find({collegeId})
         if (uniqueCollegeId.length == 0){
             res.status(400).send({status: false, message: "No College Found with this College ID"})
             return
         }
+        //Blank CollegeId Is Not Accepted  // No College Is Registered With This Id
+
 
 
         const internData = await internModel.create(data)
